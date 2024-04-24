d8e71486e73fe9d7d417c711ef39f4564ee448b9
SIDIDEV <sidimariko61@gmail.com>
2024-04-24 10:25:01 +0100

    Counter with react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
+    const [person, setPerson] = useState({
+        firstname: 'sidi',
+        lastname: 'sidi',
+        age : 18
+    })
+
+    const increment = () => {
+        setPerson({...person, age: person.age++})
+    }
+
+  return <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <p>La personne : {person.firstname} à {person.age} ans.</p>
+          <button onClick={increment}>{`Agmenter l'age`}</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </>
 }
 
 export default App
