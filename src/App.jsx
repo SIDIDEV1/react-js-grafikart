0a2b6048874c85f973c9d8258e2a7003b507716b
SIDIDEV <sidimariko61@gmail.com>
2024-04-25 21:46:13 +0100

    Custom hook react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
-import {useEffect, useRef, useState} from "react";
-import {Input} from "./components/forms/Input.jsx";
+import {useState} from "react";
+
+function useToggle(initial) {
+    const [state, setState] = useState(initial)
+    const toggleState = () => setState(v => !v)
+
+    return [state, toggleState]
+}
 
 function App() {
-    const ref = useRef(null)
-    const [prefix, setPrefix] = useState('')
-    ref.current = prefix
+    const [checked, toggleCheck] = useToggle()
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            console.log(ref.current)
-        }, 1000);
+    return <div className="p-4 max-w-2xl mx-auto">
+        <input type="checkbox" checked={checked} onChange={toggleCheck}/>
 
-        return () => {
-            clearInterval(timer)
-        }
-    }, []);
-    return <div className="p-4 max-2xl mx-auto">
-        <Input type="text" value={prefix} onChange={setPrefix}/>
-        <p ref={ref}>n Strict Mode, React will call your calculation function twice in order
-            to help you find accidental
-            impurities. This is development-only behavior and does not affect production. If your calculation function
-            is pure (as it should be), this should not affect your logic. The result from one of the calls will be
-            ignored.</p>
+        {checked && 'Je suis coché'}
     </div>
 
 }
