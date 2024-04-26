c832348420f60cdda2e96c03fe56dad17973d93c
SIDIDEV <sidimariko61@gmail.com>
2024-04-26 16:42:11 +0100

    teleport with reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
-import {useFetch} from "./components/hooks/useFetch.js";
+import {createPortal} from "react-dom";
 
 function App() {
-    // https://jsonplaceholder.typicode.com/posts?_limit=8&_delay=3000
+    return <div className=" relative bg-blue-100 m-10 h-[300px] overflow-hidden overflow-y-auto">
+        <p>
+            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.
+            Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme
+            assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait
+            que survivre cinq
+        </p>
+        <p>
+            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.
+            Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme
+            assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait
+            que survivre cinq
+        </p>
+        <p>
+            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.
+            Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme
+            assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait
+            que survivre cinq
+        </p>
+        <p>
+            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.
+            Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme
+            assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait
+            que survivre cinq
+        </p>
+        <p>
+            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.
+            Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme
+            assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait
+            que survivre cinq
+        </p>
 
-    const {
-        loading,
-        data,
-        errors
-    } = useFetch('https://jsonplaceholder.typicode.com/posts?_limit=8&_delay=3000')
-
-    return <div className="p-4 max-w-2xl mx-auto">
-        {loading && <div>Loading...</div>}
-        {data &&
-            <div>
-                {JSON.stringify(data)}
-            </div>
-        }
-        {errors && <div className="text-2xl text-red-400">{errors.toString()}</div>}
+        <Modal/>
     </div>
+}
 
+
+function Modal() {
+    return createPortal(
+        <div className="absolute bg-white top-0 right-0 p-4 border border-2">
+            <h2>Mon modal</h2>
+        </div>, document.body
+    )
 }
 
 export default App
